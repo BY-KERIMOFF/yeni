d4eb541a68c086479d8cd22a3a217647a1d1e4c7
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-27 05:24:11 +0400

    Update json_to_m3u.py

--- json_to_m3u.py
+++ json_to_m3u.py
@@ -1,7 +1,10 @@
 import json
+import os
 
 # JSON faylını oxumaq
-with open("channels.json", "r", encoding="utf-8") as f:
+json_path = os.path.join(os.path.dirname(__file__), "channels.json")
+
+with open(json_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # M3U faylını yaratmaq
@@ -11,7 +14,8 @@ for channel in data["channels"]:
     m3u_content += f'#EXTINF:-1, {channel["name"]}\n{channel["url"]}\n\n'
 
 # M3U faylını yazmaq
-with open("playlist.m3u", "w", encoding="utf-8") as f:
+m3u_path = os.path.join(os.path.dirname(__file__), "playlist.m3u")
+with open(m3u_path, "w", encoding="utf-8") as f:
     f.write(m3u_content)
 
 print("✅ M3U faylı yaradıldı: playlist.m3u")
