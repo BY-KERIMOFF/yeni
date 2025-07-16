3bee559a0492c902f17b802c8d5ec92f39137b67
LOST-LOVERS <tebrizkerimli8794@gmail.com>
2025-07-17 02:25:50 +0400

    Update and rename yeni/fetch_m3u8.py to yeni/yeni/bykerimoff_python2.py

--- yeni/yeni/bykerimoff_python2.py
+++ yeni/yeni/bykerimoff_python2.py
@@ -1,21 +1,14 @@
 import json
 
-# JSON faylının adı
 json_file = "channels.json"
-
-# M3U faylının adı
 m3u_file = "playlist.m3u"
 
 try:
-    # JSON faylını aç və oxu
     with open(json_file, "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    # Yeni M3U faylı yarat
     with open(m3u_file, "w", encoding="utf-8") as m3u:
         m3u.write("#EXTM3U\n\n")
-
-        # Kanalları əlavə et
         for channel in data["channels"]:
             m3u.write(f"#EXTINF:-1, {channel['name']}\n")
             m3u.write(f"{channel['url']}\n\n")
